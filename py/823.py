ac32ff837f6af3060b4967012f7d5dda520c20c9
hexing <he.xing@hgnyjs.com>
2019-08-24 11:21:40 +0800

    8 24

--- py/823.py
+++ py/823.py
@@ -143,3 +143,27 @@ b = 9
 swap(a, b)
 print("交换结束后,变量a的值是",\
     a,"变量b的值是:",b)
+
+
+def test() :
+    age = 20
+    #直接访问age局部变量
+    print(age) #输出20
+    #访问函数局部范围的"变量数组"
+    print(locals()) #{'age':20}
+    #通过函数局部范围的"变量数组"访问age变量的值
+    locals()['age'] = 12
+    #再次访问age变量的值
+    print('xxx', age) #依然输出20
+    #通过globals函数修改x全局变量
+    globals()['x'] = 19
+x = 5
+y = 20
+print(globals()) #
+#通过全局访问locals函数,访问的是全局变量的'变量数组"
+print(locals())
+#直接访问x全局变量
+print(x) #5
+#通过去哪聚变量的"变量数组"来访问x全局变量
+print(globals()['x'])  #5
+#通过
