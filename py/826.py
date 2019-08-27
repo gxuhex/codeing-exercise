37fdf9ce97b978013b9e109b86949f25811cf438
hexing <he.xing@hgnyjs.com>
2019-08-27 11:41:32 +0800

    item test

--- py/826.py
+++ py/826.py
@@ -303,6 +303,58 @@ def funB():
     print('B')
 print(funB)
 
+@funA
+def funB() :
+    print('B')
+print(funB)   #fkit
+
+def foo(fn) :
+    #定义一个嵌套函数
+    def bar(*args) :
+        print("===1===", args)
+        n = args[0]
+        print('===2===', n * (n - 1))
+        #查看传给foo函数的fn函数
+        print(fn.__name__)
+        fn(n * (n - 1))
+        print(" * " * 16)
+        return fn(n * (n - 1))
+    return bar
+
+'''
+下面的装饰效果相关于: foo(my_test),
+my_test将会替换(装饰)成该语句的返回值
+由于foo()函数返回bar函数,因此funB就是bar
+'''
+@foo
+def my_test(a) :
+    print("===my_test函数===", a)
+#打印my_test函数,将看到实际上是bar函数
+print(my_test)
+#下面代码看上去是调用my_test()函数, 其实是调用bar()函数
+my_test(10)
+my_test(6, 5)
+
+class Item :
+    #  直接在类空间放置执行性质代码
+    print('正在定义Item类')
+    for a in range(10) :
+        if a % 2 == 0 :
+            print('这个是偶数: ', a)
+        else :
+            print('这个是奇数: ', a)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
